63ad887238cf1672d8e1531353ffa101d1a14621
Ethan Pineda <joshuapineda66@gmail.com>
2024-05-01 19:54:13 -0500

    settig up project lol

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,15 @@
 /// <reference types="vitest" />s
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   test: { globals: true, environment: 'jsdom' },
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
