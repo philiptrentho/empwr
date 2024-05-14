6e39c8b4c50f47b41ae7e1db14548843fcda476c
Ruiyang Peng <107452997+RYAN200010@users.noreply.github.com>
2024-05-14 16:22:51 -0500

    meeting time chart

--- web/src/pages/OrganizationView.tsx
+++ web/src/pages/OrganizationView.tsx
@@ -1,7 +1,11 @@
+import MeetingTimeChart from "@/components/MeetingTimeChart/MeetingTimeChart";
+
 export default function OrganizationView() {
   return (
-    <div>
-      <h1>OrganizationView</h1>
+    <div className="relative">
+      <div className="absolute top-0 left-0">
+        <MeetingTimeChart />
+      </div>
     </div>
   );
 }
