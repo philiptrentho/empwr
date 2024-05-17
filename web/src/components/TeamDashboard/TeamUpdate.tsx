713d0c9941fd0c09ccd1d78e393df3a1827b4330
Robert Yang <RobertYang2023@u.northwestern.edu>
2024-05-16 23:18:47 -0500

    Bug Fixes

--- web/src/components/TeamDashboard/TeamUpdate.tsx
+++ web/src/components/TeamDashboard/TeamUpdate.tsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from 'react';
 import { Team } from '../../types/interfaces/types';
 import BarChart from '@/components/TeamDashboard/BarChart';
--- web/src/pages/TeamView.tsx
+++ web/src/pages/TeamView.tsx
@@ -35,58 +35,47 @@ export default function TeamView() {
   );
 
   return (
-    <div>
-      <div className="p-5 space-y-4">
-        <div>
-          <h1 className="text-lg font-semibold">Your Results</h1>
-          <h2 className="text-xl font-bold">Quantitatively Managed</h2>
-          <p className="text-gray-600">
-            Teams rely on metrics and advanced tools for decision-making, automating
-            testing, and fostering continuous delivery practices...
-          </p>
-        </div>
-        <h1 className="font-sans font-bold text-3xl mb-20">Teams</h1>
-        <div className="font-sans text-2xl flex flex-row space-x-4">
-          <div>Following </div>
-          <div>Explore </div>
-          <div>Invitation </div>
-        </div>
-        <br></br>
-        <input
-          type="text"
-          placeholder="Search meetings..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="border border-gray-300 rounded px-3 py-2 mb-4 w-2/5"
-        />
-        <div className="text-md font-sans">{teamsData.length} Teams</div>
-        <br></br>
-        <div className="flex-grow">
-          <div>
-            <TeamList teams={filteredTeams} />
-          </div>
-        </div>
+    <div className="flex flex-row">
+  <div className="flex-grow">
+    <div className="p-5 space-y-4">
+      <div>
+        <h1 className="text-lg font-semibold">Your Results</h1>
+        <h2 className="text-xl font-bold">Quantitatively Managed</h2>
+        <p className="text-gray-600">
+          Teams rely on metrics and advanced tools for decision-making, automating
+          testing, and fostering continuous delivery practices...
+        </p>
       </div>
-      <div className="h-screen w-1/5">
-        <div className="font-bold text-2xl">Team Updates</div>
-
-        <div className="h-full flex-grow">
-          <div className="h-full">
-            <TeamUpdate
-              teams={filteredTeams}
-              // @ts-expect-error
-              onChange={(e: unknown) => setSearchQuery(e.target.value)}
-              className="border border-gray-300 rounded px-3 py-2 mb-4"
-            />
-          </div>
-        </div>
-
-        <div className="flex-grow">
-          <div>
-            <TeamList teams={filteredTeams} />
-          </div>
+      <h1 className="font-sans font-bold text-3xl mb-20">Teams</h1>
+      <div className="font-sans text-2xl flex flex-row space-x-4">
+        <div>Following</div>
+        <div>Explore</div>
+        <div>Invitation</div>
+      </div>
+      <input
+        type="text"
+        placeholder="Search meetings..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+        className="border border-gray-300 rounded px-3 py-2 mb-4 w-2/5"
+      />
+      <div className="text-md font-sans">{teamsData.length} Teams</div>
+      <div className="flex-grow">
+        <div>
+          <TeamList teams={filteredTeams} />
         </div>
       </div>
     </div>
+  </div>
+  <div className="h-screen w-1/5">
+    <div className="p-5">
+      <div className="font-bold text-2xl">Team Updates</div>
+      <div className="h-full">
+        <TeamUpdate teams={teamsData} />
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
