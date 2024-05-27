1acf5aff396a47a0c68fa0c64f355c61bbf45b99
martink12 <91845242+martink12@users.noreply.github.com>
2024-05-27 15:07:30 -0500

    Add new card for DetailedTeamView

--- web/src/components/DetailedTeamView/DetailedTeamView.tsx
+++ web/src/components/DetailedTeamView/DetailedTeamView.tsx
@@ -1,10 +1,12 @@
-import { Teammate, detailedTeam } from '@/types/interfaces/types';
+import { useEffect, useState } from 'react';
+
+import { detailedTeam, Teammate } from '@/types/interfaces/types';
 
 import Card from '../Card/Card';
+import { fetchTeamDetails } from '../Firebase/detailedTeamData';
+import InfoCard from '../InfoCard/InfoCard';
 import ProgressBar from '../ProgressBar/ProgressBar';
 import TeammateList from '../TeammateList/TeammateList';
-import { useEffect, useState } from 'react';
-import { fetchTeamDetails } from '../Firebase/detailedTeamData';
 
 export default function DetailedTeamView() {
   const [team, setTeam] = useState<detailedTeam | null>(null);
@@ -23,25 +25,33 @@ export default function DetailedTeamView() {
     <div>
       {team === null && <div>Loading...</div>}
       {team && (
-      <><div className="p-5 space-y-4">
-          <div>
-            <h1 className="text-lg font-semibold">Your Results</h1>
-            <h2 className="text-xl font-bold">Quantitatively Managed</h2>
-            <p className="text-gray-600">
-              Teams rely on metrics and advanced tools for decision-making, automating
-              testing, and fostering continuous delivery practices...
-            </p>
-            <ProgressBar level={team.maturity} maxLevel={5} />
+        <>
+          <div className="p-5 space-y-4">
+            <div>
+              <h1 className="text-lg font-semibold">Your Results</h1>
+              <h2 className="text-xl font-bold">Quantitatively Managed</h2>
+              <p className="text-gray-600">
+                Teams rely on metrics and advanced tools for decision-making, automating
+                testing, and fostering continuous delivery practices...
+              </p>
+              <ProgressBar level={team.maturity} maxLevel={5} />
+            </div>
+            <div className="grid grid-cols-2 gap-4">
+              <Card
+                title="Invited Participants"
+                value={team.invitedParticipants.length}
+              />
+              <Card title="Contributors" value={team.contributors.length} />
+              <Card title="Emerging Themes" value={team.emergingThemes.length} />
+              <Card title="Recommendations" value={team.recommendations.length} />
+            </div>
           </div>
-          <div className="grid grid-cols-2 gap-4">
-            <Card title="Invited Participants" value={team.invitedParticipants.length} />
-            <Card title="Contributors" value={team.contributors.length} />
-            <Card title="Emerging Themes" value={team.emergingThemes.length} />
-            <Card title="Recommendations" value={team.recommendations.length} />
-          </div>
-        </div><div className="max-w-full overflow-x-auto p-4">
+          <div className="max-w-full overflow-x-auto p-4 flex flex-col md:flex-row md:flex-wrap space-x-6 space-y-6">
+            <InfoCard />
             <TeammateList teammates={team.userStats} />
-          </div></>) }
+          </div>
+        </>
+      )}
     </div>
   );
 }
