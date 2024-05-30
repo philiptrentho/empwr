0c2bcf39ca98abe6c903428aa34c3b461a4736c8
cesar ades <cesaradese@gmail.com>
2024-05-29 22:42:15 -0500

    Revert "org function 1.0"
    This reverts commit e7ed17854a0831071054d0ae5b55774477e38002.

--- web/src/components/Firebase/organizationData.ts
+++ web/src/components/Firebase/organizationData.ts
@@ -41,32 +41,4 @@ export const getTeamInfo = async (organizationId: string) => {
         throw error;
     }
 }
-
-export const fetchOrgMeetings = async (organizationId: string) => {
-    try {
-        const organizationRef = doc(db, organizationCollection, organizationId);
-        const organizationDoc = await getDoc(organizationRef);
-        if(organizationDoc.exists()) {
-            const organizationData = organizationDoc.data();
-            const teams = organizationData.teams;
-            const meetings: Meeting[] = [];
-            for (let i = 0; i < teams.length; i++) {
-                const teamRef = teams[i];
-                const teamDoc = await getDoc(teamRef);
-                if (teamDoc.exists()) {
-                    const meetingRef = collection(db, meetingCollection);
-                    const q = query(meetingRef, where("team", "==", teamRef));
-                    const querySnapshot = await getDocs(q);
-                    querySnapshot.forEach((doc) => {
-                        meetings.push(doc.data() as Meeting);
-                    });
-                }
-            }
-        }
-        return meetings;
-    }
-    catch (error) {
-        console.error("Error fetching meetings:", error);
-        return [];
-    }
-};
+  
